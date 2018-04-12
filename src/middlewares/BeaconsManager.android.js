26be16ba00d3b201ea3681796849be97400c7f66
Ivshin Aleksei <ivshinalekseim@mail.ru>
2018-04-12 13:29:50 +0400

    Draft

--- src/middlewares/BeaconsManager.android.js
+++ src/middlewares/BeaconsManager.android.js
@@ -6,12 +6,90 @@ import beaconActions from "../actions/BeaconActions";
 import { BeaconActionTypes } from "../actions/actionsTypes";
 import { REGION } from "../constants";
 
+import { NativeModules, DeviceEventEmitter, NativeEventEmitter } from "react-native";
+
+const { RNBluetoothManager } = NativeModules;
+
 const debouncedCleanFunction = _.debounce(dispatch => dispatch(beaconsChanged([])), 10000, {
     leading: false,
     trailing: true,
 });
 
-let isRanging = false;
+class BeaconsManager {
+    constructor(dispatch) {
+        this.isRangingRequested = false;
+        this.isRanging = false;
+        this.dispatch = dispatch;
+
+        this.beaconsChangesSubscription = null;
+        this.bleStatusSubscription = null;
+    }
+
+    startRanging() {
+        isRangingRequested = true;
+        return new Promise((resolve, reject) => {
+            BluetoothStatus.state()
+                .then(isEnabled => {
+                    console.log("BluetoothStatus>>>>>isEnabled", isEnabled);
+                    if (isEnabled) {
+                        Beacons.detectIBeacons();
+
+                        Beacons.startRangingBeaconsInRegion(REGION)
+                            .then(() => {
+                                this.isRanging = true;
+                                this._subscribeOnBeaconsChanges();
+                            })
+                            .catch(err => console.log(`Beacon ranging not started, error ${err}`));
+                    } else {
+                        Alert.alert(null, "Включите Bluetooth в настройках", [{ text: "OK" }], {
+                            cancelable: true,
+                        });
+                    }
+                })
+                .catch(error => console.log("BluetoothStatus>>>>>Error", error));
+        });
+    }
+
+    stopRanging() {
+        isRangingRequested = false;
+    }
+
+    _subscribeOnBleStatusChanges = () => {
+        this.bleStatusSubscription = new NativeEventEmitter(RNBluetoothManager).addListener(
+            "bluetoothStatus",
+            (manager, responseArray) => {
+                const bluetoothState = responseArray[0];
+                if (bluetoothState === "on") {
+                    if (this.isRangingRequested && !this.isRanging) {
+                        this.startRanging();
+                    }
+                } else if (bluetoothState === "off") {
+                    this.startRanging();
+                }
+            }
+        );
+    };
+
+    _subscribeOnBeaconsChanges = () => {
+        this.beaconsChangesSubscription = DeviceEventEmitter.addListener(
+            "beaconsDidRange",
+            data => {
+                if (data.beacons && data.beacons.length != 0) {
+                    this.dispatch(beaconActions.beaconsChanged(data.beacons));
+                    debouncedCleanFunction(this.dispatch);
+                }
+                store.dispatch(beaconActions.searching(!data.beacons || data.beacons.length == 0));
+            }
+        );
+    };
+
+    _unSubscribeFromBeaconsChanges = () => {
+        if (this.beaconsChangesSubscription) {
+            this.beaconsChangesSubscription.remove();
+            this.beaconsChangesSubscription = null;
+        }
+    };
+}
 
 export default store => {
     return next => action => {
